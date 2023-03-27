746e8b7369c242758e2af9138ec564ac58c2803e
brendan.craven <brendan.craven@student.howest.be>
2023-03-27 15:05:23 +0200

    refactor the loop for multiprocessing

--- ping.py
+++ ping.py
@@ -1,27 +1,30 @@
 import os
+from multiprocessing.pool import ThreadPool
+
+pool_size = 5
+
+
+def ping_ip(ip_add):
+    response = os.popen(f"ping -n 1 {ip_add} ").read()
+
+    if not ("Request timed out." or "unreachable") in response:
+        print(response)
+        f = open("ip_output.txt", "a")
+        f.write(str(ip_add) + '\n')
+        f.close()
+
 
 with open("ip_list_test.txt") as file:
     ip_list = file.read()
     ip_list = ip_list.splitlines()
-    print(f" {ip_list}  \n")
 
 with open("ip_output.txt", "w") as file:
     pass
 
-# ping for each ip in the file
+pool = ThreadPool(pool_size)
+
 for ip in ip_list:
-    # pinging each IP address 4 times
-    response = os.popen(f"ping -n 1 {ip} ").read()
+    pool.apply_async(ping_ip, (ip,))
 
-    # save some ping output details to output file
-    if not ("Request timed out." or "unreachable") in response:
-        print(response)
-        f = open("ip_output.txt", "a")
-        f.write(str(ip) + '\n')
-        f.close()
-        # print output file to screen
-
-with open("ip_output.txt") as file:
-    output = file.read()
-    f.close()
-    print(output)
+pool.close()
+pool.join()
